1bfa5c8f803261cbb0f7862ac9649af2cad8a6a9
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2024-05-21 12:49:14 +0100

    fix:removed empty layout file

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from "../../tetfund/public/styles/layout.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className={styles.layoutParent}>
+        <div>
           <Navbar />
           {children}
           <Footer />
