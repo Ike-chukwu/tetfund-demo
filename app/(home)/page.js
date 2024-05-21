183d945f64f6f37a61a3bed70da2d72e39956472
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2024-05-21 12:41:11 +0100

    fix:removed all lenis dependencies

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -7,37 +7,19 @@ import Partners from "../components/home/Partners/Partners";
 import LatestPosts from "../components/home/LatestPosts/LatestPosts";
 import VideoSection from "../components/home/VideoSection/VideoSection";
 import Testimonials from "../components/home/Testimonials/Testimonials";
-import { ReactLenis, useLenis } from "libs/lenis";
 import { useEffect } from "react";
-export * from "lenis/react";
 import gsap from "gsap";
 export default function Home() {
-  const lenisRef = useRef();
-
-  useEffect(() => {
-    function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000);
-    }
-
-    gsap.ticker.add(update);
-
-    return () => {
-      gsap.ticker.remove(update);
-    };
-  });
-
   return (
-    <ReactLenis  ref={lenisRef} autoRaf={false}>
-      <div>
-        <HeroSection />
-        <Mission />
-        <Vision />
-        <Events />
-        <Partners />
-        <LatestPosts />
-        <VideoSection />
-        <Testimonials />
-      </div>
-    </ReactLenis>
+    <div>
+      <HeroSection />
+      <Mission />
+      <Vision />
+      <Events />
+      <Partners />
+      <LatestPosts />
+      <VideoSection />
+      <Testimonials />
+    </div>
   );
 }
